5bc2c06c402854ad37c40b6a6aba856aa5a1c414
nakul465 <nakularora465@gmail.com>
2026-07-24 15:43:24 +0530

    basic practice problem on Arithamtic operators

---
+++ BasicProblems/BasicProblems/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+prob4(1);
+static void prob1(){
+    Console.WriteLine("Welcome to Bridgelabz!");
+}
+static void prob2(int num1,int num2)
+{
+    Console.WriteLine(num1 + num2);
+}
+static void prob3(int cel)
+{
+    Console.WriteLine((double)(cel * 9 / 5) + 32);
+}
+static void prob4(int radius)
+{
+    Console.WriteLine(3.14 * radius * radius);
+}
+static void prob5()
+{
+
+}
+static void prob6()
+{
+
+}
+
