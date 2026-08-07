c5b632386dd6738fe239bbc98d33c410759ba1d4
nakul465 <nakularora465@gmail.com>
2026-08-07 23:41:10 +0530

    Practice problems on RgularExpressions

---
+++ RegExpression/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RegExpression;
+//Console.WriteLine("Hello, World!");
+Console.WriteLine(ValidateUserName.IsValidUserName("Nakullllll_1234"));
+
---
+++ RegExpression/ValidateUserName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegExpression
+{
+	public class ValidateUserName
+	{
+		public static bool IsValidUserName(string userName)
+		{
+            string pattern = @"^(?=.{5,15}$)[A-Za-z]\w+$";
+            bool valid = Regex.IsMatch(userName, pattern);
+			return valid;
+		}
+	}
+}
+
