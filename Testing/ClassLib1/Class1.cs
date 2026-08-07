09e3b3bf94ecb086a986c8cf9554cc0df088a285
nakul465 <nakularora465@gmail.com>
2026-08-07 23:54:15 +0530

    Practice problems on UnitTesting

---
+++ Testing/ClassLib1/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassLib1;
+public class Class1
+{
+    public double add(double num1,double num2)
+    {
+        return num1 + num2;
+    }
+    public double subt(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+    public double multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+    public double divide(double num1, double num2)
+    {
+        return num1 / num2;
+    }
+    public bool isPrime(int num)
+    {
+        if (num <= 0) return false;
+        if (num <= 3) return true;
+        for(int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
+
---
+++ Testing/Test1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using ClassLib1;
+
+namespace Test1;
+
+public class Tests
+{
+    private Class1 c;
+    [SetUp]
+    public void Setup()
+    {
+        c = new Class1();
+    }
+
+    [TestCase(2, 3, 5)]
+    [TestCase(3, 3, 6)]
+    [TestCase(5, 3, 8)]
+    public void Test1(int a,int b,int expected)
+    {
+        double result = c.add(a, b);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    [TestCase(12,false)]
+    [TestCase(13, true)]
+    [TestCase(111, false)]
+    public void PrimeNumberChecker(int a,bool expected)
+    {
+        bool res=c.isPrime(a);
+        Assert.That(expected, Is.EqualTo(res));
+    }
+
+}
