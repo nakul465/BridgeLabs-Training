c1b60495f005b0127b23c5b62da11a6977ce533b
nakul465 <nakularora465@gmail.com>
2026-08-10 17:46:17 +0530

    Music Streaming System

---
+++ MusicStreamingSystem/MusicSystem.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Reflection;
+using static MusicStreamingSystem.MusicSystem;
+
+namespace MusicStreamingSystem
+{
+	public class MusicSystem
+	{
+		interface IDownloadable
+		{
+			void download();
+		}
+		public abstract class Content
+		{
+			public string title { get; set; }
+			public int duration { get; set; }
+			public Artist singer;
+			public Content(string title, int duration,Artist singer)
+			{
+				this.duration = duration;
+				this.title = title;
+				this.singer = singer;
+			}
+			public abstract void displayDetails();
+
+		}
+		public class Track:Content, IDownloadable
+        {
+			public string genre { get; set; }
+            public Track(string title, int duration, Artist singer) : base(title, duration, singer)
+            {
+				this.genre = "basic track";
+            }
+            public Track(string title, int duration, Artist singer, string genre) : base(title, duration, singer)
+			{
+				this.genre = genre;
+			}
+            public override void displayDetails()
+			{
+				Console.WriteLine($"\nTitle : {title}\nDuration : {duration}\nArtist : {singer.name}\nGenre : {genre}");
+			}
+			public void download()
+			{
+				Console.WriteLine($"Downloading Track Title : {title}");
+			}
+        }
+		public class Podcast : Content
+        {
+			public Podcast(string title, int duration, Artist singer) : base(title, duration, singer)
+            {
+            }
+            public override void displayDetails()
+            {
+                Console.WriteLine($"\nTitle : {title}\nDuration : {duration}\nArtist : {singer.name}");
+            }
+        }
+        public class Artist
+        {
+            public string name { get; }
+            List<Content> content;
+            public Artist(string name)
+            {
+                content = new List<Content>();
+                this.name = name;
+            }
+            public void addContent(Content content)
+            {
+                this.content.Add(content);
+            }
+            public void displayAllContent()
+            {
+                foreach (Content c in content)
+                {
+                    Console.WriteLine($"Title : {c.title}\nDuration : {c.duration}\nArtist : {this.name}\n");
+                }
+            }
+        }
+        public class PlayList
+		{
+			private string name;
+			List<Content> playlist;
+			public PlayList(string name)
+			{
+				this.name = name;
+				playlist = new List<Content>();
+            }
+			public void addContent(Content c)
+			{
+				playlist.Add(c);
+			}
+            public void displayContens()
+			{
+                foreach (Content c in playlist)
+                {
+                    Console.WriteLine($"\nTitle : {c.title}\nDuration : {c.duration}\nArtist : {c.singer.name}\n");
+					if(c is Track)
+					{
+						Console.WriteLine($"Genre : {((Track)c).genre}");
+					}
+                }
+            }
+		}
+		public static class MusicPlayer
+		{
+			public static void playMusic(Content content)
+			{
+				Console.WriteLine($"Now playing {content.title} by {content.singer.name}");
+			}
+			public static void SharedMusicPlayer(Listener l1,Listener l2,Content content)
+			{
+
+				Console.WriteLine($"Now {l1.Name} and {l2.Name} are playing {content.title}");
+			}
+		}
+		public class Listener
+		{
+			private string name;
+			private int uid;
+			public string Name
+			{
+				get
+				{
+					return name;
+				}
+			}
+			public Listener(string name,int uid)
+			{
+				this.uid = uid;
+				this.name = name;
+			}
+			public void displayDetails()
+			{
+				Console.WriteLine($"name : {name}\nuid : {uid}");
+			}
+		}
+	}
+}
+
---
+++ MusicStreamingSystem/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MusicStreamingSystem;
+using static MusicStreamingSystem.MusicSystem;
+Artist arijit = new Artist("Arijit");
+Artist atif = new Artist("Atif Aslam");
+Artist ranveer = new Artist("Ranveer Allahbadia");
+
+Track t1 = new Track("Song1", 2, arijit);
+Track t2 = new Track("Song2", 2, atif);
+
+Podcast p1 = new Podcast("Why Do humans Exist", 25, ranveer);
+Podcast p2 = new Podcast("Death", 35, ranveer);
+
+PlayList paylist1 = new PlayList("All Content");
+PlayList paylist2 = new PlayList("All songs");
+PlayList paylist3 = new PlayList("All podcasts");
+
+Listener l1 = new Listener("Nakul",1);
+Listener l2 = new Listener("Manish", 1);
+
+paylist1.addContent(t1);
+paylist1.addContent(t2);
+paylist1.addContent(p1);
+paylist1.addContent(p2);
+
+arijit.addContent(t1);
+
+atif.addContent(t2);
+
+ranveer.addContent(p1);
+ranveer.addContent(p2);
+
+paylist2.addContent(t1);
+paylist2.addContent(t2);
+
+paylist3.addContent(p1);
+paylist3.addContent(p2);
+
+paylist1.displayContens();
+paylist2.displayContens();
+paylist3.displayContens();
+
+t1.displayDetails();
+p1.displayDetails();
+
+Console.WriteLine("\n\nArijit:");
+arijit.displayAllContent();
+Console.WriteLine("\n\natif:");
+atif.displayAllContent();
+Console.WriteLine("\n\nranveer:");
+ranveer.displayAllContent();
+
+MusicPlayer.SharedMusicPlayer(l1, l2, t1);
