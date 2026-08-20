becd74dfca59af41049ec348a7d95a130dc334dc
nakul465 <nakularora465@gmail.com>
2026-08-20 11:56:42 +0530

    Practcir questions on complrxity

---
+++ AlgorithmComplexity/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmComplexity;
+//Console.WriteLine(SearchATargetInDataSet.LinearSearch());
+Console.WriteLine(SearchATargetInDataSet.BinarySearch());
+Console.WriteLine("Hello, World!");
---
+++ AlgorithmComplexity/SearchATargetInDataSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace AlgorithmComplexity
+{
+	public class SearchATargetInDataSet
+	{
+		
+        static int[] arr = Enumerable.Range(1, 1_000_000).ToArray();
+		static int n =0;
+        public static int LinearSearch()
+		{
+			for(int i = 0; i < arr.Length; i++)
+			{
+				if (arr[i] == n) return i;
+			}
+			return 0;
+		}
+
+		public static int BinarySearch()
+		{
+			int start = 0, end = arr.Length - 1;
+			while (start <= end)
+			{
+				int mid = start + (end - start) / 2;
+				if (arr[mid] == n)
+				{
+					return mid;
+				}
+				else if (arr[mid]>n)
+				{
+					end = mid - 1;
+				}
+				else
+				{
+					start = mid + 1;
+				}
+			}
+			return -1;
+		}
+	}
+}
+
