58bada1ea4a364db0cf4d32cca3a81e56f65a9e2
nakul465 <nakularora465@gmail.com>
2026-08-31 12:25:12 +0530

    Practice problems on JSON data handling

--- JSONDataHandling/JSONHANDLING.cs
+++ JSONDataHandling/JSONHANDLING.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
 namespace JSONDataHandling
 {
 	public class JSONHANDLING
 	{
-		public JSONHANDLING()
-		{
-		}
-	}
+        static void CreateStudentJson()
+        {
+            JObject student = new JObject
+            {
+                ["name"] = "Nakul",
+                ["age"] = 20,
+                ["subjects"] = new JArray
+        {
+            "C#",
+            "Java",
+            "DBMS"
+        }
+            };
+
+            Console.WriteLine(student);
+        }
+    }
 }
 
